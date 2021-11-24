fcfc2002b1db51d72bda99aeb549b43063477efb
ace <beautifuldiandian@qq.com>
2021-11-24 15:21:41 +0800

    feat: 添加 登录功能 和 service worker 模拟后端

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const isFalsy = (value: any) => (value === 0 ? false : !value); // 排除 0
+const isFalsy = (value: unknown) => (value === 0 ? false : !value); // 排除 0
 
 export const cleanObject = (obj: object) => {
   const result = { ...obj };
@@ -23,7 +23,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
